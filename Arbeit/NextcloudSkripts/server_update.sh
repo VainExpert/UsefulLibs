902950e8ef3060240e0753acb402204df271211f
Tom Maier <tom02.me14@gmail.com>
2022-12-16 17:18:32 +0100

    added correct beginning to .sh-files

--- Arbeit/NextcloudSkripts/server_update.sh
+++ Arbeit/NextcloudSkripts/server_update.sh
@@ -1,4 +1,6 @@
-sudo apt-get update
+#!/bin/bash
+
+apt-get update
 echo "\n--------------\n Halfway There \n--------------\n"
-sudo apt-get dist-upgrade
+apt-get dist-upgrade
 echo "\n--------------\n Done \n--------------\\n"
