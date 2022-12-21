9205a977e5a77fccfbe34078ea835272aeed25b7
Tom Maier <tom02.me14@gmail.com>
2022-12-21 16:57:16 +0100

    fixed errors in scripts, now working

--- Arbeit/NextcloudSkripts/nextcloud_storage.sh
+++ Arbeit/NextcloudSkripts/nextcloud_storage.sh
@@ -5,21 +5,21 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-freep = $(df -h | awk 'NR == 2{printf $5}' | cut -d '%' -f 1)
-frees = $(df -h | awk 'NR == 2{printf $4}' | cut -d 'G' -f 1)
+freep=$(df -h | awk 'NR == 9{printf $5}' | cut -d '%' -f 1)
+frees=$(df -h | awk 'NR == 9{printf $4}' | cut -d 'G' -f 1)
 #/dev/mapper/vg_data-srv_nextcloud_data xxxG xxG xxxG xx% /srv/nextcloud_data
-echo "\n--------------\n There are $freep % on nextcloud available \n--------------\n"
+echo -e "\n--------------\n There are $freep % on the Nextcloud available \n--------------\n"
 
-if [ $free -lt 60 ]
+if [ $freep -lt 60 ]
   then 
-    echo "\n--------------\n No resize needed \n--------------\n"
-    echo "\n--------------\n Done \n--------------\n"
+    echo -e "\n--------------\n No resize needed \n--------------\n"
+    echo -e "\n--------------\n Done \n--------------\n"
 
     exit
 
 else 
-    available = $(vgdisplay vg_data | awk 'NR == 2{printf $5}' | cut -d 'GiB' -f 1 | cut -d " " -f 5)
-    echo "\n--------------\n There are $available GiB Space still available \n--------------\n"
+    available=$(sudo vgdisplay vg_data | awk 'NR == 19{printf $7}')
+    echo -e "\n--------------\n There are $available GiB Space still available \n--------------\n"
     #Free PE / Size xxxxxx / xxx,xx GiB
 
     if [ -z "$1" ]
@@ -27,8 +27,8 @@ else
         echo -e "\n----------------------------\n No size given using, Resizing with +50GB \n----------------------------\n"
         lvresize -r -L +50G-e  /dev/mapper/vg_data-srv_nextcloud_data
         echo "\n----------------------------\n Succesfully resized with +50GB \n----------------------------\n"
-        newspace = $available - 50
-        newfree = $frees + 50
+        newspace=$available - 50
+        newfree=$frees + 50
         echo -e "\n----------------------------\n There are $newspace GiB Space still available \n----------------------------\n"
         echo -e "\n----------------------------\n There are $newfree GB Space available on the NextCloud \n----------------------------\n"
 
@@ -37,19 +37,20 @@ else
         echo -e "\n----------------------------\n Given size greater then available space, Resizing with +50GB \n----------------------------\n"
         lvresize -r -L +50G /dev/mapper/vg_data-srv_nextcloud_data
         echo -e "\n----------------------------\n Succesfully resized with +50GB \n----------------------------\n"
-        newspace = $available - 50
-        newfree = $frees + 50
+        newspace=$available - 50
+        newfree=$frees + 50
         echo -e "\n----------------------------\n There are $newspace GiB Space still available \n----------------------------\n"
         echo -e "\n----------------------------\n There are $newfree GB Space available on the NextCloud \n----------------------------\n"
 
     else 
         lvresize -r -L +$1G /dev/mapper/vg_data-srv_nextcloud_data
         echo -e "\n----------------------------\n Succesfully resized with +$1 \n----------------------------\n"
-        newspace = $available - $1
-        newfree = $frees + $1
+        newspace=$available - $1
+        newfree=$frees + $1
         echo -e "\n----------------------------\n There are $newspace GiB Space still available \n----------------------------\n"
         echo -e "\n----------------------------\n There are $newfree GB Space available on the NextCloud \n----------------------------\n"
 
     fi
 fi
+
 echo -e "\n----------------------------\n Done \n----------------------------\n"
--- Arbeit/NextcloudSkripts/nextcloud_update.sh
+++ Arbeit/NextcloudSkripts/nextcloud_update.sh
@@ -8,17 +8,18 @@ fi
 if [ -z "$1" ]
   then
     echo "No argument supplied"
+    exit
 fi
 
-if [ $PWD -ne "/srv/www/cloud.saralon.com" ]
+if [ $PWD != "/srv/www/cloud.saralon.com" ]
   then
-    cd /srv/www/cloud.saralon.com
-
+    echo -e "Please execute in directory /srv/www/cloud.saralon.com/"
+    exit
 fi
 
--u www-data php /srv/www/cloud.saralon.com/htdocs/occ maintenance:mode --on
+sudo -u www-data php /srv/www/cloud.saralon.com/htdocs/occ maintenance:mode --on
 echo -e "\n----------------------------\n Server in Maintenance-Mode \n----------------------------\n"
--u sys.backup usr/local/bin/backup_db.sh /var/backups/backup_db
+sudo -u sys.backup usr/local/bin/backup_db.sh /var/backups/backup_db
 echo -e "\n----------------------------\n Database backuped \n----------------------------\n"
 wget $1
 tar -xf nextcloud*.tar.bz2
@@ -26,15 +27,15 @@ echo -e "\n----------------------------\n New Release downloaded \n-------------
 cp -a htdocs/.well-known nextcloud
 cp -a htdocs/config nextcloud
 echo -e "\n----------------------------\n Config and Extras copied \n----------------------------\n"
-chwon -R www-data:www-data nextcloud/*
-chwon root:root nextcloud
+chown -R www-data:www-data nextcloud/*
+chown root:root nextcloud
 echo -e "\n----------------------------\n Ownerships changed \n----------------------------\n"
 mv hdocs htdocs.old
 mv nextcloud htdocs
 echo -e "\n----------------------------\n New Version active \n----------------------------\n"
--u www-data php /srv/www/cloud.saralon.com/htdocs/occ upgrade
+sudo -u www-data php /srv/www/cloud.saralon.com/htdocs/occ upgrade
 echo -e "\n----------------------------\n Datastructure upgrade \n----------------------------\n"
--u www-data php /srv/www/cloud.saralon.com/htdocs/occ maintenance:mode --off
+sudo -u www-data php /srv/www/cloud.saralon.com/htdocs/occ maintenance:mode --off
 echo -e "\n----------------------------\n Server not anymore in Maintenance-Mode \n----------------------------\n"
 echo -e "Check now in admin overview for errors and warnigs and deal with them\n"
 echo -e "after that is fixed run\nsudo nextcloud_deleteold.sh\n"
