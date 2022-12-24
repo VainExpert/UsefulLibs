a7cc523d227392f9ed9445cce03b00500ba0bcd2
Tom Maier <tom02.me14@gmail.com>
2022-12-24 07:57:01 +0100

    added regex fo rdownload-link, fixed echo strings to uified format

--- Arbeit/NextcloudSkripts/nextcloud_deleteold.sh
+++ Arbeit/NextcloudSkripts/nextcloud_deleteold.sh
@@ -7,5 +7,5 @@ fi
 
 sudo rm -rf /srv/www/cloud.saralon.com/htdocs.old
 sudo rm /srv/www/cloud.saralon.com/nextcloud*.tar.bz2
-echo -e "\n----------------------------\n Removed successfully \n----------------------------\n"
-echo -e "\n----------------------------\n Done \n----------------------------\n"
+echo -e "----------------------------\n Removed successfully \n----------------------------"
+echo -e "----------------------------\n Done \n----------------------------"
--- Arbeit/NextcloudSkripts/nextcloud_update.sh
+++ Arbeit/NextcloudSkripts/nextcloud_update.sh
@@ -5,40 +5,40 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-if [ -z "$1" ]
+if ! [[ "$1" =~ ^https://download\.nextcloud\.com/server/releases/nextcloud-([0-9]+(\.[0-9]+)+)\.tar\.bz2$ ]]
   then
-    echo "No argument supplied"
+    echo "Make sure you supplied a correct Download-Link for the .tar.bz2 of the official NextCloud Website"
     exit
 fi
 
 if [ $PWD != "/srv/www/cloud.saralon.com" ]
   then
-    echo -e "Please execute in directory /srv/www/cloud.saralon.com/"
+    echo "Please execute in directory /srv/www/cloud.saralon.com/"
     exit
 fi
 
 sudo -u www-data php /srv/www/cloud.saralon.com/htdocs/occ maintenance:mode --on
-echo -e "\n----------------------------\n Server in Maintenance-Mode \n----------------------------\n"
+echo -e "----------------------------\n Server in Maintenance-Mode \n----------------------------"
 sudo -u sys.backup usr/local/bin/backup_db.sh /var/backups/backup_db
-echo -e "\n----------------------------\n Database backuped \n----------------------------\n"
+echo -e "----------------------------\n Database backuped \n----------------------------"
 sudo tar -czf /srv/www/cloud.saralon.com.bak.tar.gz /srv/www/cloud.saralon.com
-echo -e "\n----------------------------\n Webroot of Nextcloud saved \n----------------------------\n"
+echo -e "----------------------------\n Webroot of Nextcloud saved \n----------------------------"
 sudo wget $1
 sudo tar -xf nextcloud*.tar.bz2
-echo -e "\n----------------------------\n New Release downloaded \n----------------------------\n"
+echo -e "----------------------------\n New Release downloaded \n----------------------------"
 sudo cp -a htdocs/.well-known nextcloud
 sudo cp -a htdocs/config nextcloud
-echo -e "\n----------------------------\n Config and Extras copied \n----------------------------\n"
+echo -e "----------------------------\n Config and Extras copied \n----------------------------"
 sudo chown -R www-data:www-data nextcloud/*
 sudo chown root:root nextcloud
-echo -e "\n----------------------------\n Ownerships changed \n----------------------------\n"
+echo -e "----------------------------\n Ownerships changed \n----------------------------"
 sudo mv hdocs htdocs.old
 sudo mv nextcloud htdocs
-echo -e "\n----------------------------\n New Version active \n----------------------------\n"
+echo -e "----------------------------\n New Version active \n----------------------------"
 sudo -u www-data php /srv/www/cloud.saralon.com/htdocs/occ upgrade
-echo -e "\n----------------------------\n Datastructure upgrade \n----------------------------\n"
+echo -e "----------------------------\n Datastructure upgrade \n----------------------------"
 sudo -u www-data php /srv/www/cloud.saralon.com/htdocs/occ maintenance:mode --off
-echo -e "\n----------------------------\n Server not anymore in Maintenance-Mode \n----------------------------\n"
-echo -e "Check now in admin overview for errors and warnigs and deal with them\n"
-echo -e "after that is fixed run\nsudo nextcloud_deleteold.sh\n"
-echo -e "\n----------------------------\n Done \n----------------------------\n"
+echo -e "----------------------------\n Server not anymore in Maintenance-Mode \n----------------------------"
+echo -e "----------------------------\nCheck now in admin overview for errors and warnings and deal with them\n"
+echo -e "after that is fixed run\n> sudo nextcloud_deleteold.sh\n----------------------------"
+echo -e "----------------------------\n Done \n----------------------------"
